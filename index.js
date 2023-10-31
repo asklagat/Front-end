035122fe0cd49464a7e8923c24a3c80cbdf813f8
kevin_lagat <asklagat@gmail.com>
2023-10-31 23:58:35 +0300

    text content

---
+++ index.js
@@ -0,0 +1,7 @@
+let firstName = "Kevin";
+let hobby = "Playing chess";
+let age = 21;
+
+document.getElementById("p1").textContent =`My name is ${firstName}`;
+document.getElementById("p2").textContent = `I love ${hobby}`;
+document.getElementById("p3").textContent = `I am ${age} years old`;
